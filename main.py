88ae5e0819eb326ec30352d645a39060053d9d4a
SubNerd <74598401+SubNerd@users.noreply.github.com>
2023-01-30 21:38:32 +0530

    Seamless, smooth chunks!

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import math
 import pyglet
-import random
+import opensimplex
 import numpy as np
-from pyvista import PolyData
 from pyglet.gl import *
 from player import FirstPersonCamera
 
@@ -21,25 +20,31 @@ class LoDChunk:
         self.position = position
         self.size = size
         self.mesh = np.array([])
+        self.heightmap = np.zeros((size + 2, size + 2))
         self.batch = pyglet.graphics.Batch()
         
     def generate(self):
+        # Generate the heightmap
+        for x in range(self.position[0] - 1, self.position[0] + self.size + 1):
+            for z in range(self.position[1] - 1, self.position[1] + self.size + 1):
+                index_x = x - self.position[0] + 1
+                index_z = z - self.position[1] + 1
+                self.heightmap[index_x, index_z] = opensimplex.noise2(x / 16, z / 16) * 4
+        
         for x in range(self.position[0], self.position[0] + self.size):
             for z in range(self.position[1], self.position[1] + self.size):
-                self.mesh = np.append(self.mesh, [x, 0, z])
-                
-        # Generate 3D triangle mesh from this point cloud
-        pointcloud = PolyData(self.mesh)
-        mesh = pointcloud.delaunay_2d()
-        
-        for index, point in enumerate(mesh.points):
-            mesh.points[index][1] = random.randint(-5, 4)
-        
-        # Convert to pyglet vertex list
-        self.mesh = mesh.points.flatten()
+                self.mesh = np.append(self.mesh, [
+                    x, self.heightmap[x - self.position[0], z - self.position[1]], z,
+                    x + 1, self.heightmap[x - self.position[0] + 1, z - self.position[1]], z,
+                    x, self.heightmap[x - self.position[0], z - self.position[1] + 1], z + 1,
+
+                    x + 1, self.heightmap[x - self.position[0] + 1, z - self.position[1]], z,
+                    x + 1, self.heightmap[x - self.position[0] + 1, z - self.position[1] + 1], z + 1,
+                    x, self.heightmap[x - self.position[0], z - self.position[1] + 1], z + 1
+                ])
         
         # Update the batch
-        self.batch.add_indexed(len(self.mesh) // 3, GL_TRIANGLE_FAN, None, range(len(self.mesh) // 3), ('v3f', self.mesh))
+        self.batch.add_indexed(len(self.mesh) // 3, GL_LINES, None, range(len(self.mesh) // 3), ('v3f', self.mesh))
         
     def draw(self):
         self.batch.draw()
